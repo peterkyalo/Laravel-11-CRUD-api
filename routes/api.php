497d2c29c28c653ac55b09c7b94dffc72d2e4778
Peter Kyalo <peterkyalo69@gmaik.com>
2024-09-25 13:43:22 +0300

    Created CRUD logic

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+//Post routes
+Route::apiResource('posts', PostController::class);
+

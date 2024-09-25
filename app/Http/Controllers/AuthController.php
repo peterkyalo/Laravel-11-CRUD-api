f01ac6379b268d07d9858679379094bbdf1cef8d
Peter Kyalo <peterkyalo69@gmaik.com>
2024-09-25 14:32:09 +0300

    Added register and login logic with AccessToken

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        return 'register';
+        $fields = $request->validate([
+            'name' => ['required','string','max:255'],
+            'email' => ['required','email', 'unique:users'],
+            'password' => ['required', 'confirmed'],
+        ]);
+
+        $user = User::create($fields);
+        //create user token
+        $token = $user->createToken($request->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function login(Request $request)
     {
-        return 'login';
+        $request->validate([
+            'email' => ['required','email', 'exists:users'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+           return [
+            'message' => 'The provided credentials are incorrect.',
+           ];
+          }
+          //create user token
+          $token = $user->createToken($user->name);
+          return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function logout()

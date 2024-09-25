84c0fec3a1a6e769998ea7542f1c2bfa8850095a
Peter Kyalo <peterkyalo69@gmaik.com>
2024-09-25 19:38:37 +0300

    Added policy into Post Controller

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 
 //Post routes
